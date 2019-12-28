70611778b86a17549135c505221d1d3ccbae7b1e
abiyogakhusus@gmail.com <Yogaanakibusendangke2>
2019-12-28 11:41:40 +0700

    pengubahan dalam file migration

--- system/app/Http/Controllers/HomeController.php
+++ system/app/Http/Controllers/HomeController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Kamar;
 
 class HomeController extends Controller
 {
     public function index(){
+       $kamar =  Kamar::get();
+       dd($kamar->toArray(  ));
         return view('pages.home');
     }
+
+
 }
---
+++ system/app/Models/Kamar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kamar extends Model
+{
+    //
+    Protected $table = "spek_Kamar";
+
+
+}
